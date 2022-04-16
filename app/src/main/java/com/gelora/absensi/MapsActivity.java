300f6340b42c0db15a526ce2d32a53dc5fb927a5
ADIT-PC <aditiawadani6@gmail.com>
2022-04-16 10:31:44 +0700

    Bismillah Update App

--- app/src/main/java/com/gelora/absensi/MapsActivity.java
+++ app/src/main/java/com/gelora/absensi/MapsActivity.java
@@ -516,7 +516,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     public void onErrorResponse(VolleyError error) {
                         // error
                         Log.d("Error.Response", error.toString());
-                        //connectionFailed();
+                        // connectionFailed();
                     }
                 }
         )
