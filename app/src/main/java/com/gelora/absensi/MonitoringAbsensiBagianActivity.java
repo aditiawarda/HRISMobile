447e9f3982471c3ec3e0de3501d772666090a178
ADIT-PC <aditiawadani6@gmail.com>
2022-06-29 13:42:41 +0700

    perbaikan fungsi shift driver

--- app/src/main/java/com/gelora/absensi/MonitoringAbsensiBagianActivity.java
+++ app/src/main/java/com/gelora/absensi/MonitoringAbsensiBagianActivity.java
@@ -29,11 +29,13 @@ import com.gelora.absensi.model.DataMonitoringKehadiranBagian;
 import com.gelora.absensi.model.DataPulangCepat;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.takisoft.datetimepicker.DatePickerDialog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -42,12 +44,12 @@ import java.util.Map;
 
 public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
 
-    TextView jumlahKaryawanTV, currentDateTV, currentDayTV, namaDepartemen, namaBagian, monitorDataHadir, monitorDataTidakHadir;
-    String currentDay = "";
+    TextView jumlahKaryawanTV, currentDateTV, namaDepartemen, namaBagian, monitorDataHadir, monitorDataTidakHadir;
+    String currentDay = "", dateChoiceForHistory;
     SharedPrefManager sharedPrefManager;
     ImageView dataTidakHadirLoading, dataHadirLoading, loadingDataKehadiranBagian;
     SwipeRefreshLayout refreshLayout;
-    LinearLayout backBTN, homeBTN, loadingDataKehadiranPart;
+    LinearLayout backBTN, homeBTN, loadingDataKehadiranPart, choiceDateBTN, noDataHadirBagian;
 
     private RecyclerView dataKehadiranBagianRV;
     private DataMonitoringKehadiranBagian[] dataMonitoringKehadiranBagians;
@@ -61,7 +63,6 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
         sharedPrefManager = new SharedPrefManager(this);
         jumlahKaryawanTV = findViewById(R.id.jumlah_karyawan_tv);
         currentDateTV = findViewById(R.id.current_date);
-        currentDayTV = findViewById(R.id.current_day);
         namaDepartemen = findViewById(R.id.nama_departemen);
         namaBagian = findViewById(R.id.nama_bagian);
         monitorDataHadir = findViewById(R.id.monitor_data_hadir);
@@ -73,6 +74,10 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
         homeBTN = findViewById(R.id.home_btn);
         loadingDataKehadiranPart = findViewById(R.id.loading_data_kehadiran_bagian_part);
         loadingDataKehadiranBagian = findViewById(R.id.loading_data_kehadiran_bagian);
+        choiceDateBTN = findViewById(R.id.choice_date);
+        noDataHadirBagian = findViewById(R.id.no_data_hadir_bagian);
+
+        dateChoiceForHistory = getDate();
 
         Glide.with(getApplicationContext())
                 .load(R.drawable.loading_dots)
@@ -98,7 +103,9 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                dateChoiceForHistory = getDate();
 
+                noDataHadirBagian.setVisibility(View.GONE);
                 dataHadirLoading.setVisibility(View.VISIBLE);
                 dataTidakHadirLoading.setVisibility(View.VISIBLE);
                 monitorDataHadir.setVisibility(View.GONE);
@@ -130,6 +137,13 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MonitoringAbsensiBagianActivity.this, MapsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        choiceDateBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker();
             }
         });
 
@@ -170,8 +184,8 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
                                     }
                                 }, 1000);
 
-
                                 if (hadir.equals("0")) {
+                                    noDataHadirBagian.setVisibility(View.VISIBLE);
                                     dataKehadiranBagianRV.setVisibility(View.GONE);
                                     loadingDataKehadiranPart.setVisibility(View.GONE);
                                 } else {
@@ -179,6 +193,7 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
                                     new Handler().postDelayed(new Runnable() {
                                         @Override
                                         public void run() {
+                                            noDataHadirBagian.setVisibility(View.GONE);
                                             loadingDataKehadiranPart.setVisibility(View.GONE);
                                             dataKehadiranBagianRV.setVisibility(View.VISIBLE);
                                         }
@@ -212,7 +227,7 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("id_bagian", sharedPrefManager.getSpIdDept());
-                params.put("tanggal", getDate());
+                params.put("tanggal", dateChoiceForHistory);
                 return params;
             }
         };
@@ -223,43 +238,42 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     private void dateLive(){
-        currentDayTV.setText(currentDay+",");
         switch (getDateM()) {
             case "01":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Januari " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Januari " + getDateY());
                 break;
             case "02":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Februari " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Februari " + getDateY());
                 break;
             case "03":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Maret " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Maret " + getDateY());
                 break;
             case "04":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " April " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " April " + getDateY());
                 break;
             case "05":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Mei " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Mei " + getDateY());
                 break;
             case "06":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Juni " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Juni " + getDateY());
                 break;
             case "07":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Juli " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Juli " + getDateY());
                 break;
             case "08":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Agustus " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Agustus " + getDateY());
                 break;
             case "09":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " September " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " September " + getDateY());
                 break;
             case "10":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Oktober " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Oktober " + getDateY());
                 break;
             case "11":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " November " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " November " + getDateY());
                 break;
             case "12":
-                currentDateTV.setText(String.valueOf(Integer.parseInt(getDateD()))+ " Desember " + getDateY());
+                currentDateTV.setText(currentDay +", "+ String.valueOf(Integer.parseInt(getDateD()))+ " Desember " + getDateY());
                 break;
             default:
                 currentDateTV.setText("Not found!");
@@ -326,6 +340,219 @@ public class MonitoringAbsensiBagianActivity extends AppCompatActivity {
         }
 
         dateLive();
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private void datePicker(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            android.icu.util.Calendar cal = android.icu.util.Calendar.getInstance();
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            DatePickerDialog dpd = new DatePickerDialog(MonitoringAbsensiBagianActivity.this, (view1, year, month, dayOfMonth) -> {
+
+                dateChoiceForHistory = String.format("%d", year)+"-"+String.format("%02d", month + 1)+"-"+String.format("%02d", dayOfMonth);
+                String input_date = dateChoiceForHistory;
+                SimpleDateFormat format1=new SimpleDateFormat("yyyy-MM-dd");
+                Date dt1= null;
+                try {
+                    dt1 = format1.parse(input_date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                DateFormat format2 = new SimpleDateFormat("EEE");
+                DateFormat getweek = new SimpleDateFormat("W");
+                String finalDay = format2.format(dt1);
+                String week = getweek.format(dt1);
+                String hariName = "";
+
+                if (finalDay.equals("Mon") || finalDay.equals("Sen")) {
+                    hariName = "Senin";
+                } else if (finalDay.equals("Tue") || finalDay.equals("Sel")) {
+                    hariName = "Selasa";
+                } else if (finalDay.equals("Wed") || finalDay.equals("Rab")) {
+                    hariName = "Rabu";
+                } else if (finalDay.equals("Thu") || finalDay.equals("Kam")) {
+                    hariName = "Kamis";
+                } else if (finalDay.equals("Fri") || finalDay.equals("Jum")) {
+                    hariName = "Jumat";
+                } else if (finalDay.equals("Sat") || finalDay.equals("Sab")) {
+                    hariName = "Sabtu";
+                } else if (finalDay.equals("Sun") || finalDay.equals("Min")) {
+                    hariName = "Minggu";
+                }
+
+                String dayDate = input_date.substring(8,10);
+                String yearDate = input_date.substring(0,4);;
+                String bulanValue = input_date.substring(5,7);
+                String bulanName;
+
+                switch (bulanValue) {
+                    case "01":
+                        bulanName = "Januari";
+                        break;
+                    case "02":
+                        bulanName = "Februari";
+                        break;
+                    case "03":
+                        bulanName = "Maret";
+                        break;
+                    case "04":
+                        bulanName = "April";
+                        break;
+                    case "05":
+                        bulanName = "Mei";
+                        break;
+                    case "06":
+                        bulanName = "Juni";
+                        break;
+                    case "07":
+                        bulanName = "Juli";
+                        break;
+                    case "08":
+                        bulanName = "Agustus";
+                        break;
+                    case "09":
+                        bulanName = "September";
+                        break;
+                    case "10":
+                        bulanName = "Oktober";
+                        break;
+                    case "11":
+                        bulanName = "November";
+                        break;
+                    case "12":
+                        bulanName = "Desember";
+                        break;
+                    default:
+                        bulanName = "Not found!";
+                        break;
+                }
+
+                currentDateTV.setText(hariName+", "+String.valueOf(Integer.parseInt(dayDate))+" "+bulanName+" "+yearDate);
+
+                dataHadirLoading.setVisibility(View.VISIBLE);
+                dataTidakHadirLoading.setVisibility(View.VISIBLE);
+                monitorDataHadir.setVisibility(View.GONE);
+                monitorDataTidakHadir.setVisibility(View.GONE);
+                loadingDataKehadiranPart.setVisibility(View.VISIBLE);
+                dataKehadiranBagianRV.setVisibility(View.GONE);
+                noDataHadirBagian.setVisibility(View.GONE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getKehadiranBagian();
+                    }
+                }, 1000);
+
+            }, cal.get(android.icu.util.Calendar.YEAR), cal.get(android.icu.util.Calendar.MONTH), cal.get(android.icu.util.Calendar.DATE));
+            dpd.show();
+        } else {
+            int y = Integer.parseInt(getDateY());
+            int m = Integer.parseInt(getDateM());
+            int d = Integer.parseInt(getDateD());
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            DatePickerDialog dpd = new DatePickerDialog(MonitoringAbsensiBagianActivity.this, (view1, year, month, dayOfMonth) -> {
+
+                dateChoiceForHistory = String.format("%d", year)+"-"+String.format("%02d", month + 1)+"-"+String.format("%02d", dayOfMonth);
+                String input_date = dateChoiceForHistory;
+                SimpleDateFormat format1=new SimpleDateFormat("yyyy-MM-dd");
+                Date dt1= null;
+                try {
+                    dt1 = format1.parse(input_date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                DateFormat format2 = new SimpleDateFormat("EEE");
+                DateFormat getweek = new SimpleDateFormat("W");
+                String finalDay = format2.format(dt1);
+                String week = getweek.format(dt1);
+                String hariName = "";
+
+                if (finalDay.equals("Mon") || finalDay.equals("Sen")) {
+                    hariName = "Senin";
+                } else if (finalDay.equals("Tue") || finalDay.equals("Sel")) {
+                    hariName = "Selasa";
+                } else if (finalDay.equals("Wed") || finalDay.equals("Rab")) {
+                    hariName = "Rabu";
+                } else if (finalDay.equals("Thu") || finalDay.equals("Kam")) {
+                    hariName = "Kamis";
+                } else if (finalDay.equals("Fri") || finalDay.equals("Jum")) {
+                    hariName = "Jumat";
+                } else if (finalDay.equals("Sat") || finalDay.equals("Sab")) {
+                    hariName = "Sabtu";
+                } else if (finalDay.equals("Sun") || finalDay.equals("Min")) {
+                    hariName = "Minggu";
+                }
+
+                String dayDate = input_date.substring(8,10);
+                String yearDate = input_date.substring(0,4);;
+                String bulanValue = input_date.substring(5,7);
+                String bulanName;
+
+                switch (bulanValue) {
+                    case "01":
+                        bulanName = "Januari";
+                        break;
+                    case "02":
+                        bulanName = "Februari";
+                        break;
+                    case "03":
+                        bulanName = "Maret";
+                        break;
+                    case "04":
+                        bulanName = "April";
+                        break;
+                    case "05":
+                        bulanName = "Mei";
+                        break;
+                    case "06":
+                        bulanName = "Juni";
+                        break;
+                    case "07":
+                        bulanName = "Juli";
+                        break;
+                    case "08":
+                        bulanName = "Agustus";
+                        break;
+                    case "09":
+                        bulanName = "September";
+                        break;
+                    case "10":
+                        bulanName = "Oktober";
+                        break;
+                    case "11":
+                        bulanName = "November";
+                        break;
+                    case "12":
+                        bulanName = "Desember";
+                        break;
+                    default:
+                        bulanName = "Not found";
+                        break;
+                }
+
+                currentDateTV.setText(hariName+", "+String.valueOf(Integer.parseInt(dayDate))+" "+bulanName+" "+yearDate);
+
+                dataHadirLoading.setVisibility(View.VISIBLE);
+                dataTidakHadirLoading.setVisibility(View.VISIBLE);
+                monitorDataHadir.setVisibility(View.GONE);
+                monitorDataTidakHadir.setVisibility(View.GONE);
+                loadingDataKehadiranPart.setVisibility(View.VISIBLE);
+                dataKehadiranBagianRV.setVisibility(View.GONE);
+                noDataHadirBagian.setVisibility(View.GONE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getKehadiranBagian();
+                    }
+                }, 1000);
+
+            }, y,m,d);
+            dpd.show();
+        }
+
 
     }
 
