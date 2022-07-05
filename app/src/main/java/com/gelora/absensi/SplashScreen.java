ca29bd51d3e43b8671ac9052ae47d12d6f38fad4
ADIT-PC <aditiawadani6@gmail.com>
2022-07-05 17:40:35 +0700

    bug handling

--- app/src/main/java/com/gelora/absensi/SplashScreen.java
+++ app/src/main/java/com/gelora/absensi/SplashScreen.java
@@ -384,7 +384,7 @@ public class SplashScreen extends AppCompatActivity {
                             String close_btn = response.getString("close_btn");
 
                             if (status.equals("Success")){
-                                String currentVersion = "1.1.30";
+                                String currentVersion = "1.1.31";
                                 if (!currentVersion.equals(version) && popup.equals("1")){
                                     statusUpdateLayout = "1";
 
