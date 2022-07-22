1632bb340aad43316448dd66825e40d7ce8c0ff2
Aditia Warda <aditiawardani6@gmail.com>
2022-07-22 15:24:56 +0700

    Update App

--- app/src/main/java/com/gelora/absensi/UserActivity.java
+++ app/src/main/java/com/gelora/absensi/UserActivity.java
@@ -2129,7 +2129,7 @@ public class UserActivity extends AppCompatActivity {
                             String popup = response.getString("pop_up");
 
                             if (status.equals("Success")){
-                                String currentVersion = "1.1.32"; //harus disesuaikan
+                                String currentVersion = "1.1.33"; //harus disesuaikan
                                 if (!currentVersion.equals(version) && popup.equals("1")){
                                     updateBTN.setVisibility(View.VISIBLE);
                                     updateBTN.setOnClickListener(new View.OnClickListener() {
