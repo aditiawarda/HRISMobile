4ba08a74a6ee98c0f1769909d9b5ee9642e51ce0
Aditia Warda <aditiawardani6@gmail.com>
2022-12-21 14:59:05 +0700

    Update HRIS Mobile Gelora

--- app/src/main/java/com/gelora/absensi/MapsActivity.java
+++ app/src/main/java/com/gelora/absensi/MapsActivity.java
@@ -171,7 +171,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
     RequestQueue requestQueue;
-    String appVersion = "1.4.5";
+    String appVersion = "1.4.6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/gelora/absensi/SplashScreen.java
+++ app/src/main/java/com/gelora/absensi/SplashScreen.java
@@ -384,7 +384,7 @@ public class SplashScreen extends AppCompatActivity {
                             String close_btn = response.getString("close_btn");
 
                             if (status.equals("Success")){
-                                String currentVersion = "1.4.5";
+                                String currentVersion = "1.4.6";
                                 if (!currentVersion.equals(version) && popup.equals("1")){
 
                                     refreshPart.animate()
