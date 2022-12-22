5dbea8cee9f6b5a4d1ef98e0a7a60934a3204cb8
Aditia Warda <aditiawardani6@gmail.com>
2022-12-22 14:40:38 +0700

    Update HRIS Mobile Gelora

--- app/src/main/java/com/gelora/absensi/CompanyActivity.java
+++ app/src/main/java/com/gelora/absensi/CompanyActivity.java
@@ -1,5 +1,7 @@
 package com.gelora.absensi;
 
+import static android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
@@ -7,15 +9,18 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CompanyActivity extends AppCompatActivity {
 
     LinearLayout backBTN, homeBTN;
+    TextView p1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
