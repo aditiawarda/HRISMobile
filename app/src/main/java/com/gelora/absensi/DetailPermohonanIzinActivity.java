878cff507f594d1794527faab55b95acff08c33e
Aditia Warda <aditiawardani6@gmail.com>
2023-01-16 10:23:22 +0700

    Update HRIS Mobile Gelora

--- app/src/main/java/com/gelora/absensi/DetailPermohonanIzinActivity.java
+++ app/src/main/java/com/gelora/absensi/DetailPermohonanIzinActivity.java
@@ -1284,22 +1284,15 @@ public class DetailPermohonanIzinActivity extends AppCompatActivity {
     }
 
     private Bitmap getBitmapFromView(View view) {
-        //Define a bitmap with the same size as the view
         Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
-        //Bind a canvas to it
         Canvas canvas = new Canvas(returnedBitmap);
-        //Get the view's background
         Drawable bgDrawable =view.getBackground();
         if (bgDrawable!=null) {
-            //has background drawable, then draw it on the canvas
             bgDrawable.draw(canvas);
-        }   else{
-            //does not have background drawable, then draw white background on the canvas
+        } else {
             canvas.drawColor(Color.WHITE);
         }
-        // draw the view on the canvas
         view.draw(canvas);
-        //return the bitmap
         return returnedBitmap;
     }
 
