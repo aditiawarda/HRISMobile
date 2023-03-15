68468b754fac774beeddabcacc88dc168f195402
Aditia Warda <aditiawardani6@gmail.com>
2023-03-15 16:41:32 +0700

    HRIS New

--- app/src/main/java/com/gelora/absensi/SplashScreen.java
+++ app/src/main/java/com/gelora/absensi/SplashScreen.java
@@ -385,7 +385,7 @@ public class SplashScreen extends AppCompatActivity {
                             String close_btn = response.getString("close_btn");
 
                             if (status.equals("Success")){
-                                String currentVersion = "1.7.2";
+                                String currentVersion = "1.7.3";
                                 if (!currentVersion.equals(version) && popup.equals("1")){
 
                                     refreshPart.animate()
