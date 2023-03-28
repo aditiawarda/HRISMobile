3a73aa2ae9e2d235a9c061c8b80f58918149e722
Aditia Warda <aditiawardani6@gmail.com>
2023-03-28 15:09:53 +0700

    HRIS New

--- app/src/main/java/com/gelora/absensi/HumanResourceActivity.java
+++ app/src/main/java/com/gelora/absensi/HumanResourceActivity.java
@@ -2,13 +2,30 @@ package com.gelora.absensi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.gelora.absensi.support.StatusBarColorManager;
 
 public class HumanResourceActivity extends AppCompatActivity {
+
+    LinearLayout backBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_human_resource);
+
+        backBTN = findViewById(R.id.back_btn);
+
+        backBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
