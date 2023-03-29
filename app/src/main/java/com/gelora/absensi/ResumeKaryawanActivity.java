3ae67419e1c5c51234dcd33737695c2cbe069772
Aditia Warda <aditiawardani6@gmail.com>
2023-03-29 16:31:06 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/ResumeKaryawanActivity.java
+++ app/src/main/java/com/gelora/absensi/ResumeKaryawanActivity.java
@@ -1,20 +1,56 @@
 package com.gelora.absensi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class ResumeKaryawanActivity extends AppCompatActivity {
+
+    LinearLayout backBTN, actionBar;
+    SharedPrefManager sharedPrefManager;
+    SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_karyawan);
 
+        actionBar = findViewById(R.id.action_bar);
+        backBTN = findViewById(R.id.back_btn);
+
         String NIK = getIntent().getExtras().getString("NIK");
 
-        Toast.makeText(this, NIK, Toast.LENGTH_SHORT).show();
+        actionBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        refreshLayout.setColorSchemeResources(android.R.color.holo_green_dark, android.R.color.holo_blue_dark, android.R.color.holo_orange_dark, android.R.color.holo_red_dark);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.setRefreshing(false);
+                        //getDataPermohonan();
+                    }
+                }, 800);
+            }
+        });
+
+        backBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
     }
 }
