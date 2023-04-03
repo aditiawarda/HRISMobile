13b72f68dc03547ac9bae455f7bb6869424f59a7
Aditia Warda <aditiawardani6@gmail.com>
2023-04-03 09:47:16 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/FormPenilaianKaryawanActivity.java
+++ app/src/main/java/com/gelora/absensi/FormPenilaianKaryawanActivity.java
@@ -1,14 +1,59 @@
 package com.gelora.absensi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FormPenilaianKaryawanActivity extends AppCompatActivity {
+
+    LinearLayout backBTN, actionBar;
+    SharedPrefManager sharedPrefManager;
+    SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_penilaian_karyawan);
+
+        sharedPrefManager = new SharedPrefManager(this);
+        refreshLayout = findViewById(R.id.swipe_to_refresh_layout);
+        backBTN = findViewById(R.id.back_btn);
+        actionBar = findViewById(R.id.action_bar);
+
+        actionBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        refreshLayout.setColorSchemeResources(android.R.color.holo_green_dark, android.R.color.holo_blue_dark, android.R.color.holo_orange_dark, android.R.color.holo_red_dark);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.setRefreshing(false);
+
+                    }
+                }, 1000);
+            }
+        });
+
+        backBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
+
 }
