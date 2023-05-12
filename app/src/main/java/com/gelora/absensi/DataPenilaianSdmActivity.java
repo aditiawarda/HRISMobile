aacc6e92d38caddb7bebd576bfa37d2d0d59dd46
Aditia Warda <aditiawardani6@gmail.com>
2023-05-12 14:32:11 +0700

    HRIS Update

---
+++ app/src/main/java/com/gelora/absensi/DataPenilaianSdmActivity.java
@@ -0,0 +1,14 @@
+package com.gelora.absensi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataPenilaianSdmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_penilaian_sdm);
+    }
+}
