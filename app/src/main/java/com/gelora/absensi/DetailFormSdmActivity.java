6cf3d3ff2dd6ca5c4a2980985ea247d6d11a4848
Aditia Warda <aditiawardani6@gmail.com>
2023-05-29 09:26:18 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/DetailFormSdmActivity.java
+++ app/src/main/java/com/gelora/absensi/DetailFormSdmActivity.java
@@ -3,43 +3,29 @@ package com.gelora.absensi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.android.volley.toolbox.Volley;
-
-public class FormSdmActivity extends AppCompatActivity {
+public class DetailFormSdmActivity extends AppCompatActivity {
 
     LinearLayout backBTN;
     SharedPrefManager sharedPrefManager;
     SwipeRefreshLayout refreshLayout;
-    TextView titleBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_sdm);
+        setContentView(R.layout.activity_detail_form_sdm);
 
         sharedPrefManager = new SharedPrefManager(this);
         backBTN = findViewById(R.id.back_btn);
         refreshLayout = findViewById(R.id.swipe_to_refresh_layout);
-        titleBTN = findViewById(R.id.title_btn);
 
         backBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
-            }
-        });
-
-        titleBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FormSdmActivity.this, DetailFormSdmActivity.class);
-                startActivity(intent);
             }
         });
 
