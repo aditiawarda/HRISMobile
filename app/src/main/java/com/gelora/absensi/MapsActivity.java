f2f61272a99a9d0198cdd85a0d3688e16e97f921
Aditia Warda <aditiawardani6@gmail.com>
2023-08-19 09:08:29 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/MapsActivity.java
+++ app/src/main/java/com/gelora/absensi/MapsActivity.java
@@ -160,7 +160,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
     RequestQueue requestQueue;
-    String appVersion = "1.9.3";
+    String appVersion = "1.9.4";
     private StatusBarColorManager mStatusBarColorManager;
 
     private RecyclerView dataAbsensiRV;
