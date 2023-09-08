b257cec79618b9ab6404fd02440913e4b5191702
Aditia Warda <aditiawardani6@gmail.com>
2023-09-08 14:51:59 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/AllMenuActivity.java
+++ app/src/main/java/com/gelora/absensi/AllMenuActivity.java
@@ -4,22 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.gelora.absensi.adapter.AdapterPersonalNotification;
+import com.gelora.absensi.kalert.KAlertDialog;
 import com.gelora.absensi.model.DataPersonalNotification;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AllMenuActivity extends AppCompatActivity {
 
-    LinearLayout actionBar, backBTN, menuAbsensiBTN;
+    LinearLayout cutiPart, pengaduanPart, cardPart, sdmPart;
+    LinearLayout actionBar, backBTN, menuAbsensiBTN, menuIzinBTN, menuCutiBTN, menuPengaduanBTN, menuFingerBTN, menuSdmBTN, menuCardBTN;
     SharedPrefManager sharedPrefManager;
     SharedPrefAbsen sharedPrefAbsen;
     SwipeRefreshLayout refreshLayout;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +48,21 @@ public class AllMenuActivity extends AppCompatActivity {
 
         sharedPrefManager = new SharedPrefManager(this);
         sharedPrefAbsen = new SharedPrefAbsen(this);
+        requestQueue = Volley.newRequestQueue(this);
         refreshLayout = findViewById(R.id.swipe_to_refresh_layout);
         backBTN = findViewById(R.id.back_btn);
         actionBar = findViewById(R.id.action_bar);
+        cutiPart = findViewById(R.id.cuti_part);
+        cardPart = findViewById(R.id.card_part);
+        sdmPart = findViewById(R.id.sdm_part);
+        pengaduanPart = findViewById(R.id.pengaduan_part);
         menuAbsensiBTN = findViewById(R.id.menu_absensi_btn);
+        menuIzinBTN = findViewById(R.id.menu_izin_btn);
+        menuCutiBTN = findViewById(R.id.menu_cuti_btn);
+        menuPengaduanBTN = findViewById(R.id.menu_pengaduan_btn);
+        menuFingerBTN = findViewById(R.id.menu_finger_btn);
+        menuSdmBTN = findViewById(R.id.menu_sdm_btn);
+        menuCardBTN = findViewById(R.id.menu_card_btn);
 
         actionBar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +79,7 @@ public class AllMenuActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         refreshLayout.setRefreshing(false);
+                        roleMenu();
                         //getData();
                     }
                 }, 1000);
@@ -70,5 +102,283 @@ public class AllMenuActivity extends AppCompatActivity {
             }
         });
 
+        menuIzinBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sharedPrefManager.getSpIdJabatan().equals("31")){
+                    new KAlertDialog(AllMenuActivity.this, KAlertDialog.WARNING_TYPE)
+                            .setTitleText("Perhatian")
+                            .setContentText("Khusus PKL, pengajuan izin dapat langsung menghubungi HRD")
+                            .setConfirmText("    OK    ")
+                            .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                                @Override
+                                public void onClick(KAlertDialog sDialog) {
+                                    sDialog.dismiss();
+                                }
+                            })
+                            .show();
+                } else {
+                    Intent intent = new Intent(AllMenuActivity.this, FormPermohonanIzinActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        menuCutiBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllMenuActivity.this, FormPermohonanCutiActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        menuPengaduanBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new KAlertDialog(AllMenuActivity.this, KAlertDialog.WARNING_TYPE)
+                        .setTitleText("Perhatian")
+                        .setContentText("Anda akan terhubung dengan Bagian HRD")
+                        .setCancelText("    BATAL    ")
+                        .setConfirmText("  LANJUT  ")
+                        .showCancelButton(true)
+                        .setCancelClickListener(new KAlertDialog.KAlertClickListener() {
+                            @Override
+                            public void onClick(KAlertDialog sDialog) {
+                                sDialog.dismiss();
+                            }
+                        })
+                        .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                            @Override
+                            public void onClick(KAlertDialog sDialog) {
+                                sDialog.dismiss();
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        getContact();
+                                    }
+                                }, 500);
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        menuFingerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sharedPrefManager.getSpIdJabatan().equals("31")){
+                    new KAlertDialog(AllMenuActivity.this, KAlertDialog.WARNING_TYPE)
+                            .setTitleText("Perhatian")
+                            .setContentText("Khusus PKL, pengajuan fingerscan/keterangan tidak absen dapat langsung menghubungi HRD")
+                            .setConfirmText("    OK    ")
+                            .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                                @Override
+                                public void onClick(KAlertDialog sDialog) {
+                                    sDialog.dismiss();
+                                }
+                            })
+                            .show();
+                } else {
+                    Intent intent = new Intent(AllMenuActivity.this, FormFingerscanActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        roleMenu();
+
     }
+
+    private void roleMenu() {
+        if(sharedPrefManager.getSpStatusKaryawan().equals("Tetap")||sharedPrefManager.getSpStatusKaryawan().equals("Kontrak")){
+            if(sharedPrefManager.getSpStatusKaryawan().equals("Tetap")){
+                cutiPart.setVisibility(View.VISIBLE);
+                pengaduanPart.setVisibility(View.GONE);
+                //clearancePart.setVisibility(View.VISIBLE);
+                //calendarPart.setVisibility(View.GONE);
+            } else if(sharedPrefManager.getSpStatusKaryawan().equals("Kontrak")){
+                if(!sharedPrefManager.getSpTglBergabung().equals("") || !sharedPrefManager.getSpTglBergabung().equals("null")){
+                    String tanggalMulaiBekerja = sharedPrefManager.getSpTglBergabung();
+                    String tanggalSekarang = getDate();
+
+                    @SuppressLint("SimpleDateFormat")
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                    Date date1 = null;
+                    Date date2 = null;
+                    try {
+                        date1 = format.parse(tanggalSekarang);
+                        date2 = format.parse(tanggalMulaiBekerja);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    long waktu1 = date1.getTime();
+                    long waktu2 = date2.getTime();
+                    long selisih_waktu = waktu1 - waktu2;
+                    long diffDays = selisih_waktu / (24 * 60 * 60 * 1000);
+                    long diffMonths = (selisih_waktu / (24 * 60 * 60 * 1000)) / 30;
+                    long diffYears =  ((selisih_waktu / (24 * 60 * 60 * 1000)) / 30) / 12;
+
+                    if(diffMonths >= 12){
+                        cutiPart.setVisibility(View.VISIBLE);
+                        pengaduanPart.setVisibility(View.GONE);
+                    } else {
+                        cutiPart.setVisibility(View.GONE);
+                        pengaduanPart.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    cutiPart.setVisibility(View.GONE);
+                    pengaduanPart.setVisibility(View.VISIBLE);
+                }
+            }
+            //clearancePart.setVisibility(View.VISIBLE);
+            //calendarPart.setVisibility(View.GONE);
+        } else {
+            cutiPart.setVisibility(View.GONE);
+            pengaduanPart.setVisibility(View.VISIBLE);
+            if(sharedPrefManager.getSpIdCor().equals("1")){
+                if(sharedPrefManager.getSpIdJabatan().equals("29")||sharedPrefManager.getSpIdJabatan().equals("31")){
+                    //clearancePart.setVisibility(View.GONE);
+                    //calendarPart.setVisibility(View.VISIBLE);
+                } else {
+                    //clearancePart.setVisibility(View.VISIBLE);
+                    //calendarPart.setVisibility(View.GONE);
+                }
+            } else if(sharedPrefManager.getSpIdCor().equals("3")){
+                //clearancePart.setVisibility(View.GONE);
+                //calendarPart.setVisibility(View.VISIBLE);
+            }
+        }
+
+        if(sharedPrefManager.getSpIdJabatan().equals("11")||sharedPrefManager.getSpIdJabatan().equals("25")||sharedPrefManager.getSpIdJabatan().equals("3")||sharedPrefManager.getSpIdJabatan().equals("10")||sharedPrefManager.getSpNik().equals("1309131210")){
+            cardPart.setVisibility(View.GONE);
+            sdmPart.setVisibility(View.VISIBLE);
+        } else {
+            cardPart.setVisibility(View.VISIBLE);
+            sdmPart.setVisibility(View.GONE);
+        }
+    }
+
+    private void getContact() {
+        //RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        if(sharedPrefManager.getSpIdCor().equals("1")){
+            final String url = "https://geloraaksara.co.id/absen-online/api/get_contact";
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.e("PaRSE JSON", response + "");
+                            try {
+                                String bagian = response.getString("bagian");
+                                String nama = response.getString("nama");
+                                String whatsapp = response.getString("whatsapp");
+
+                                Intent webIntent = new Intent(Intent.ACTION_VIEW); webIntent.setData(Uri.parse("https://api.whatsapp.com/send?phone=+"+whatsapp+"&text="));
+                                try {
+                                    startActivity(webIntent);
+                                } catch (SecurityException e) {
+                                    e.printStackTrace();
+                                    new KAlertDialog(AllMenuActivity.this, KAlertDialog.WARNING_TYPE)
+                                            .setTitleText("Perhatian")
+                                            .setContentText("Tidak dapat terhubung ke Whatsapp, anda bisa hubungi secara langsung ke 0"+whatsapp.substring(2, whatsapp.length())+" atas nama Bapak "+nama+" bagian HRD")
+                                            .setConfirmText("    OK    ")
+                                            .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                                                @Override
+                                                public void onClick(KAlertDialog sDialog) {
+                                                    sDialog.dismiss();
+                                                }
+                                            })
+                                            .show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    new KAlertDialog(AllMenuActivity.this, KAlertDialog.ERROR_TYPE)
+                            .setTitleText("Perhatian")
+                            .setContentText("Gagal terhubung, harap periksa jaringan anda")
+                            .setConfirmText("    OK    ")
+                            .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                                @Override
+                                public void onClick(KAlertDialog sDialog) {
+                                    sDialog.dismiss();
+                                }
+                            })
+                            .show();
+                }
+            });
+
+            requestQueue.add(request);
+
+        } else if(sharedPrefManager.getSpIdCor().equals("3")){
+            final String url = "https://geloraaksara.co.id/absen-online/api/get_contact_erlass";
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.e("PaRSE JSON", response + "");
+                            try {
+                                String bagian = response.getString("bagian");
+                                String nama = response.getString("nama");
+                                String whatsapp = response.getString("whatsapp");
+
+                                Intent webIntent = new Intent(Intent.ACTION_VIEW); webIntent.setData(Uri.parse("https://api.whatsapp.com/send?phone=+"+whatsapp+"&text="));
+                                try {
+                                    startActivity(webIntent);
+                                } catch (SecurityException e) {
+                                    e.printStackTrace();
+                                    new KAlertDialog(AllMenuActivity.this, KAlertDialog.WARNING_TYPE)
+                                            .setTitleText("Perhatian")
+                                            .setContentText("Tidak dapat terhubung ke Whatsapp, anda bisa hubungi secara langsung ke 0"+whatsapp.substring(2, whatsapp.length())+" atas nama Bapak "+nama+" bagian HRD")
+                                            .setConfirmText("    OK    ")
+                                            .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                                                @Override
+                                                public void onClick(KAlertDialog sDialog) {
+                                                    sDialog.dismiss();
+                                                }
+                                            })
+                                            .show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    new KAlertDialog(AllMenuActivity.this, KAlertDialog.ERROR_TYPE)
+                            .setTitleText("Perhatian")
+                            .setContentText("Gagal terhubung, harap periksa jaringan anda")
+                            .setConfirmText("    OK    ")
+                            .setConfirmClickListener(new KAlertDialog.KAlertClickListener() {
+                                @Override
+                                public void onClick(KAlertDialog sDialog) {
+                                    sDialog.dismiss();
+                                }
+                            })
+                            .show();
+                }
+            });
+
+            requestQueue.add(request);
+
+        }
+
+    }
+
+    private String getDate() {
+        @SuppressLint("SimpleDateFormat")
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
 }
