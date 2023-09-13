d9a1601a1291883d25aa690dcbb8e89769636407
Aditia Warda <aditiawardani6@gmail.com>
2023-09-13 15:07:53 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/DetailExitClearanceActivity.java
+++ app/src/main/java/com/gelora/absensi/DetailExitClearanceActivity.java
@@ -1,21 +1,49 @@
 package com.gelora.absensi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class DetailExitClearanceActivity extends AppCompatActivity {
 
-    LinearLayout backBTN;
+    LinearLayout actionBar, backBTN;
+    SwipeRefreshLayout refreshLayout;
+    SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_exit_clearance);
 
+        sharedPrefManager = new SharedPrefManager(this);
+        refreshLayout = findViewById(R.id.swipe_to_refresh_layout);
+        actionBar = findViewById(R.id.action_bar);
         backBTN = findViewById(R.id.back_btn);
+
+        actionBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        refreshLayout.setColorSchemeResources(android.R.color.holo_green_dark, android.R.color.holo_blue_dark, android.R.color.holo_orange_dark, android.R.color.holo_red_dark);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.setRefreshing(false);
+                        //getData();
+                    }
+                }, 500);
+            }
+        });
 
         backBTN.setOnClickListener(new View.OnClickListener() {
             @Override
