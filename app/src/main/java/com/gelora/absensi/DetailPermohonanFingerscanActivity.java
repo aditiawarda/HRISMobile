d287b1d6385019eab6cba26e909edc867c2aeca9
Aditia Warda <aditiawardani6@gmail.com>
2023-12-11 08:17:36 +0700

    HRIS Update

--- app/src/main/java/com/gelora/absensi/DetailPermohonanFingerscanActivity.java
+++ app/src/main/java/com/gelora/absensi/DetailPermohonanFingerscanActivity.java
@@ -578,12 +578,6 @@ public class DetailPermohonanFingerscanActivity extends AppCompatActivity {
                                         approverTV.setText("( "+short_name_2[0].toUpperCase()+" )");
                                     }
 
-                                    // String shortName2 = nama_approver+" ";
-                                    // if(shortName2.contains(" ")){
-                                    //    shortName2 = shortName2.substring(0, shortName2.indexOf(" "));
-                                    //    approverTV.setText("( "+shortName2.toUpperCase()+" )");
-                                    // }
-
                                     jabatanApproverTV.setText(jabatan_approver);
 
                                     String status_approve_hrd = detail.getString("status_approve_hrd");
