f308d795349ec60025b1f8cba004307d0a812b1b
Aditia Warda <aditiawardani6@gmail.com>
2024-09-17 09:10:05 +0700

    update penyesuaian hris

--- app/src/main/java/com/gelora/absensi/adapter/AdapterLunchRequest.java
+++ app/src/main/java/com/gelora/absensi/adapter/AdapterLunchRequest.java
@@ -3,17 +3,16 @@ package com.gelora.absensi.adapter;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.gelora.absensi.DetailPermohonanCutiActivity;
 import com.gelora.absensi.EditLunchRequestActivity;
 import com.gelora.absensi.ListDataLunchRequestActivity;
 import com.gelora.absensi.R;
@@ -51,130 +50,133 @@ public class AdapterLunchRequest extends RecyclerView.Adapter<AdapterLunchReques
     public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, final int i) {
         final DataListLunchRequest dataList = data[i];
 
-        String dayDate = dataList.getTanggal().substring(8,10);
-        String yearDate = dataList.getTanggal().substring(0,4);
-        String bulanValue = dataList.getTanggal().substring(5,7);
-        String bulanName;
-
-        switch (bulanValue) {
-            case "01":
-                bulanName = "JAN";
-                break;
-            case "02":
-                bulanName = "FEB";
-                break;
-            case "03":
-                bulanName = "MAR";
-                break;
-            case "04":
-                bulanName = "APR";
-                break;
-            case "05":
-                bulanName = "MEI";
-                break;
-            case "06":
-                bulanName = "JUN";
-                break;
-            case "07":
-                bulanName = "JUL";
-                break;
-            case "08":
-                bulanName = "AGU";
-                break;
-            case "09":
-                bulanName = "SEP";
-                break;
-            case "10":
-                bulanName = "OKT";
-                break;
-            case "11":
-                bulanName = "NOV";
-                break;
-            case "12":
-                bulanName = "DES";
-                break;
-            default:
-                bulanName = "Not found";
-                break;
-        }
-
-        myViewHolder.tglTV.setText(dayDate+" "+bulanName+" "+yearDate);
-        myViewHolder.bagianTV.setText(dataList.getBagian());
-        myViewHolder.requesterTV.setText(dataList.getNama_requester());
-        myViewHolder.createdAtTV.setText(dataList.getCreated_at());
-        myViewHolder.jumlah1TV.setText(dataList.getPusat_siang_k());
-        myViewHolder.jumlah2TV.setText(dataList.getPusat_siang_s());
-        myViewHolder.jumlah3TV.setText(dataList.getPusat_sore());
-        myViewHolder.jumlah4TV.setText(dataList.getPusat_malam());
-        myViewHolder.jumlah5TV.setText(dataList.getGapprint_siang());
-        myViewHolder.jumlah6TV.setText(dataList.getGapprint_sore());
-        myViewHolder.jumlah7TV.setText(dataList.getGapprint_malam());
-
-        myViewHolder.expandBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(myViewHolder.expandableLayout.isExpanded()){
-                    myViewHolder.expandableLayout.collapse();
-                } else {
-                    myViewHolder.expandableLayout.expand();
-                }
-            }
-        });
-
-        @SuppressLint("SimpleDateFormat")
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = null;
-        Date date2 = null;
         try {
-            date = sdf.parse(dataList.getTanggal());
-            date2 = sdf.parse(getDate());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        long pilih = date.getTime();
-        long sekarang = date2.getTime();
+            String dayDate = dataList.getTanggal().substring(8,10);
+            String yearDate = dataList.getTanggal().substring(0,4);
+            String bulanValue = dataList.getTanggal().substring(5,7);
+            String bulanName;
 
-        if(pilih<sekarang) {
-            myViewHolder.updateBTN.setVisibility(View.GONE);
-        } else if (pilih==sekarang){
-            String jamString = getTimeNow();
-            String batasString = sharedPrefAbsen.getSpLunchRequestTimeout();
+            switch (bulanValue) {
+                case "01":
+                    bulanName = "JAN";
+                    break;
+                case "02":
+                    bulanName = "FEB";
+                    break;
+                case "03":
+                    bulanName = "MAR";
+                    break;
+                case "04":
+                    bulanName = "APR";
+                    break;
+                case "05":
+                    bulanName = "MEI";
+                    break;
+                case "06":
+                    bulanName = "JUN";
+                    break;
+                case "07":
+                    bulanName = "JUL";
+                    break;
+                case "08":
+                    bulanName = "AGU";
+                    break;
+                case "09":
+                    bulanName = "SEP";
+                    break;
+                case "10":
+                    bulanName = "OKT";
+                    break;
+                case "11":
+                    bulanName = "NOV";
+                    break;
+                case "12":
+                    bulanName = "DES";
+                    break;
+                default:
+                    bulanName = "Not found";
+                    break;
+            }
+
+            myViewHolder.tglTV.setText(dayDate+" "+bulanName+" "+yearDate);
+            myViewHolder.bagianTV.setText(dataList.getBagian());
+            myViewHolder.requesterTV.setText(dataList.getNama_requester());
+            myViewHolder.createdAtTV.setText(dataList.getCreated_at());
+            myViewHolder.jumlah1TV.setText(dataList.getPusat_siang_k());
+            myViewHolder.jumlah2TV.setText(dataList.getPusat_siang_s());
+            myViewHolder.jumlah3TV.setText(dataList.getPusat_sore());
+            myViewHolder.jumlah4TV.setText(dataList.getPusat_malam());
+            myViewHolder.jumlah5TV.setText(dataList.getGapprint_siang());
+            myViewHolder.jumlah6TV.setText(dataList.getGapprint_sore());
+            myViewHolder.jumlah7TV.setText(dataList.getGapprint_malam());
+
+            myViewHolder.expandBTN.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(myViewHolder.expandableLayout.isExpanded()){
+                        myViewHolder.expandableLayout.collapse();
+                    } else {
+                        myViewHolder.expandableLayout.expand();
+                    }
+                }
+            });
 
             @SuppressLint("SimpleDateFormat")
-            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = null;
+            Date date2 = null;
             try {
-                Date jam = format.parse(jamString);
-                Date batas = format.parse(batasString);
-
-                if (jam.before(batas)) {
-                    myViewHolder.updateBTN.setVisibility(View.VISIBLE);
-                    myViewHolder.updateBTN.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent = new Intent(mContext, EditLunchRequestActivity.class);
-                            intent.putExtra("id", dataList.getId());
-                            mContext.startActivity(intent);
-                        }
-                    });
-                } else {
-                    myViewHolder.updateBTN.setVisibility(View.GONE);
-                }
+                date = sdf.parse(dataList.getTanggal());
+                date2 = sdf.parse(getDate());
             } catch (ParseException e) {
                 e.printStackTrace();
             }
+            long pilih = date.getTime();
+            long sekarang = date2.getTime();
 
-        } else if (pilih>sekarang){
-            myViewHolder.updateBTN.setVisibility(View.VISIBLE);
-            myViewHolder.updateBTN.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(mContext, EditLunchRequestActivity.class);
-                    intent.putExtra("id", dataList.getId());
-                    mContext.startActivity(intent);
+            if(pilih<sekarang) {
+                myViewHolder.updateBTN.setVisibility(View.GONE);
+            } else if (pilih==sekarang){
+                String jamString = getTimeNow();
+                String batasString = sharedPrefAbsen.getSpLunchRequestTimeout();
+
+                @SuppressLint("SimpleDateFormat")
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                try {
+                    Date jam = format.parse(jamString);
+                    Date batas = format.parse(batasString);
+
+                    if (jam.before(batas)) {
+                        myViewHolder.updateBTN.setVisibility(View.VISIBLE);
+                        myViewHolder.updateBTN.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(mContext, EditLunchRequestActivity.class);
+                                intent.putExtra("id", dataList.getId());
+                                mContext.startActivity(intent);
+                            }
+                        });
+                    } else {
+                        myViewHolder.updateBTN.setVisibility(View.GONE);
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
                 }
-            });
-        }
 
+            } else if (pilih>sekarang){
+                myViewHolder.updateBTN.setVisibility(View.VISIBLE);
+                myViewHolder.updateBTN.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(mContext, EditLunchRequestActivity.class);
+                        intent.putExtra("id", dataList.getId());
+                        mContext.startActivity(intent);
+                    }
+                });
+            }
+        } catch (StringIndexOutOfBoundsException e){
+            Log.e("Error",e.toString());
+        }
     }
 
     @Override
